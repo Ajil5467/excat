fb9c0384414337d772a45f83dc828611bcb711f0
Ajil5467 <ajiltu5467@gmail.com>
2021-07-13 06:25:43 +0530

    update

--- script.js
+++ script.js
@@ -4,34 +4,40 @@ const fs = require("fs");
 // read file abc.txt in ut-8 format
 let processData = process.argv;
 
-if(processData[2] = "a"){
+if(processData[2] == "a"){
     let previousData = fs.readFileSync(processData[3], "utf-8");
     fs.writeFileSync(processData[3], previousData + "\n" + processData[4]);
-}else if(processData[2] = "w"){
+}else if(processData[2] == "w"){
     fs.writeFileSync(processData[3], processData[4]);
 }else{
     let data = " ";
+    let iUpdated = false;
     for(i = 2; i < processData.length; i++){
-        
+        if(!iUpdated){
+            i = i + 1;
+            iUpdated = false;
+        }
+
         //remove if empty lines
         if(processData.includes("ne")){
-            i = i + 1;
+            
             let tempData = fs.readFileSync(processData[i], "utf-8");
             let lines = tempData.split("\n");
-            console.log(lines);
+            if(tempData.includes("\r")){
+                lines = tempData.split("\r\n");
+            }
+            
+            let finalData = "";
+            for(j = 0; j < lines.length; j++){
+                if(lines[j] != ""){
+                    finalData += lines[j] + "\n";
+                }
+            }
+            data += finalData;
         }else{
                 data += "\n" + fs.readFileSync(processData[i], "utf-8");
         }
-    
+
 }
 console.log(data);
 }
-
-
-
-
-
-
-
-
-// fs.writeFileSync("def.txt", "I'm very good");
